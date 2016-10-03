b121415ca0875e2ef717ebb49aedf11f8258ac03
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-03 15:09:14 +0100

    User should be vm_user

--- site/profile/manifests/labmachine.pp
+++ site/profile/manifests/labmachine.pp
@@ -15,7 +15,7 @@ define profile::labmachine (
     size              => 'Standard_DS1_v2',
     subnet            => 'default',
     virtual_network   => 'Group IDC IDC',
-    user              => $user,
+    vm_user              => $user,
     password          => $password,
   }
 
