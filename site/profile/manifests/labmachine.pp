39d977a056738660426c95aa55d697f3ec162705
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-03 15:00:08 +0100

    Added definition for azure_vm_classic in profile::labmachine

--- site/profile/manifests/labmachine.pp
+++ site/profile/manifests/labmachine.pp
@@ -5,4 +5,18 @@ define profile::labmachine (
   $email,
 ) {
   notify { "Creating Lab Machine $username for $fullname": }
+  azure_vm_classic { "$username-idc1210":
+    ensure            => 'running',
+    cloud_service     => "$username-idc1210",
+    data_disk_size_gb => '0',
+    deployment        => "$username-idc1210",
+    endpoints         => [{'direct_server_return' => 'false', 'local_port' => '3389', 'name' => 'RDP UDP', 'protocol' => 'udp', 'public_port' => '3389'}, {'direct_server_return' => 'false', 'local_port' => '3389', 'name' => 'RDP TCP', 'protocol' => 'tcp', 'public_port' => '3389'}, {'direct_server_return' => 'false', 'local_port' => '5986', 'name' => 'WinRM', 'protocol' => 'tcp', 'public_port' => '5986'}],
+    image             => 'a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd',
+    location          => 'South Central US',
+    os_type           => 'Windows',
+    size              => 'Standard_DS1_v2',
+    subnet            => 'default',
+    virtual_network   => 'Group IDC IDC',
+  }
+
 }
