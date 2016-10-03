eb6554fc0e76f76a28cbeeb74ee9cd00933d2d49
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-03 15:05:16 +0100

    Removing OS type

--- site/profile/manifests/labmachine.pp
+++ site/profile/manifests/labmachine.pp
@@ -12,7 +12,6 @@ define profile::labmachine (
     endpoints         => [{'direct_server_return' => 'false', 'local_port' => '3389', 'name' => 'RDP UDP', 'protocol' => 'udp', 'public_port' => '3389'}, {'direct_server_return' => 'false', 'local_port' => '3389', 'name' => 'RDP TCP', 'protocol' => 'tcp', 'public_port' => '3389'}, {'direct_server_return' => 'false', 'local_port' => '5986', 'name' => 'WinRM', 'protocol' => 'tcp', 'public_port' => '5986'}],
     image             => 'a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd',
     location          => 'South Central US',
-    os_type           => 'Windows',
     size              => 'Standard_DS1_v2',
     subnet            => 'default',
     virtual_network   => 'Group IDC IDC',
